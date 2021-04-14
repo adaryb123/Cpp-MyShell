fdf9ebe68497d3b5d8ea56b5c2b121932943bc04
AdamR <adam.rybansky@centrum.sk>
2021-04-14 15:51:45 +0200

    all special characters but | working

--- Zadanie2ASM/Source.cpp
+++ Zadanie2ASM/Source.cpp
@@ -31,47 +31,101 @@ std::ostream& operator<<(std::ostream& lhs, const CommandObject& rhs)
 	for (size_t i = 0; i < rhs.m_arguments.size(); i++)
 		lhs << '\"' << rhs.m_arguments[i] << '\"' << ", ";
 	lhs << "\n";
+	lhs << "INPUT FILE: " << rhs.m_input_file_name << "\n";
+	lhs << "OUTPUT FILE: " << rhs.m_output_file_name << "\n";
 	return lhs;
 }
 
-void createCommandObjects(string input_line) {
+vector<CommandObject> createCommandObjects(string input_line) {
+
+	vector<CommandObject> result, other_objects;
 	vector<char> delimiters = { ' ','\t','\r','\n','\a' };
 	vector<char> special_characters = { '#',';','<','>','|' };
 	char escape_character = '\\';
-
-	string first_word = "";
+	string first_word = "", word = "" , input_file = "", output_file = "";
 	vector<string> other_words;
-	bool first_done = false;
+	bool first_done = false, output_file_mode = false, input_file_mode = false, filename_ended = false;
 
-	string word = "";
 	for (size_t i = 0; i < input_line.size(); i++) {
 		bool is_delimiter = false;
 		for (size_t j = 0; j < delimiters.size(); j++) 
 			if (input_line[i] == delimiters[j])
 				is_delimiter = true;
 
+		if (!is_delimiter && input_line[i] != ';' && filename_ended) break;
+
 		if (is_delimiter) {
-			if (word.empty())
-				continue;
-			else {
-				if (first_done)
-					other_words.push_back(word);
+			if ((output_file_mode && !output_file.empty()) || (input_file_mode && !input_file.empty()))
+				filename_ended = true;
+			else{
+				if (word.empty())
+					continue;
 				else {
-					first_word = word;
-					first_done = true;
+					if (first_done)
+						other_words.push_back(word);
+					else {
+						first_word = word;
+						first_done = true;
+					}
+					word = "";
 				}
-				word = "";
 			}
 		}
-		else
-			word += input_line[i];
+		else if( input_line[i] == '\\') {
+			if (i + 1 < input_line.size()) {
+				bool next_is_special = false;
+				for (size_t j = 0; j < special_characters.size(); j++)
+					if (input_line[i+1] == special_characters[j])
+						next_is_special = true;
+
+				if (next_is_special) {
+					i++;
+					word += input_line[i];
+					continue;
+				}
+				else {
+					word += '\\';
+					continue;
+				}
+			}
+		}
+		else if (input_line[i] == '#') {
+			break;
+		}
+		else if (input_line[i] == ';') {
+			string other_command = input_line.substr(i + 1);
+			other_objects = createCommandObjects(other_command);
+			break;
+		}
+		else if (input_line[i] == '>' && !output_file_mode && !input_file_mode) {
+			output_file_mode = true;
+		}
+		else if (input_line[i] == '<'&& !output_file_mode && !input_file_mode) {
+			input_file_mode = true;
+		}
+		else {
+			if (input_file_mode)
+				input_file += input_line[i];
+			else if (output_file_mode)
+				output_file += input_line[i];
+			else
+				word += input_line[i];
+		}
 	}
-	if (first_done)
-		other_words.push_back(word);
-	else 
-		first_word = word;
-		CommandObject commandObject{ first_word,other_words,"","" };
-		cout << commandObject;
+	if (word != "") {
+		if (first_done)
+			other_words.push_back(word);
+		else
+			first_word = word;
+	}
+
+
+	CommandObject commandObject{ first_word,other_words,output_file,input_file};
+	result.push_back(commandObject);
+	for (size_t i = 0; i < other_objects.size(); i++)
+		result.push_back(other_objects[i]);
+
+	return result;
 	}
 
 	/*
@@ -87,7 +141,10 @@ void createCommandObjects(string input_line) {
 
 int main(int argc, char* argv[]) {
 	string input_line;
+	vector<CommandObject> commands;
 	getline(std::cin, input_line);
-	createCommandObjects(input_line);
+	commands = createCommandObjects(input_line);
+	for (size_t i = 0; i < commands.size(); i++)
+		cout << i << ":\n" << commands[i] << "\n";
 	return 0;
 }
