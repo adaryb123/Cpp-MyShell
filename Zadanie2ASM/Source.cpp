11cb7ae216e502ed16bb1db1d12caa0cfb7fbb72
AdamR <adam.rybansky@centrum.sk>
2021-04-19 22:02:55 +0200

    execute commands working with pipes and input/ output files. But pipes make shell end after 1 command

--- Zadanie2ASM/Source.cpp
+++ Zadanie2ASM/Source.cpp
@@ -9,6 +9,8 @@
 #include <sys/socket.h>
 #include <ctype.h>
 #include <sys/un.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 
 #include <iostream>
 #include <string>
@@ -27,6 +29,7 @@ source: https://brennan.io/2015/01/16/write-a-shell-in-c/  */
 int shell_cd(char** args);
 int shell_help(char** args);
 int shell_exit(char** args);
+int shell_halt(char** args);
 
 /*
   List of builtin commands, followed by their corresponding functions.
@@ -34,13 +37,15 @@ int shell_exit(char** args);
 string builtin_str[] = {
   "cd",
   "help",
-  "exit"
+  "quit",
+  "halt",
 };
 
 int (*builtin_func[]) (char**) = {
   &shell_cd,
   &shell_help,
-  &shell_exit
+  &shell_exit,
+  &shell_halt
 };
 
 int shell_num_builtins() {
@@ -97,6 +102,12 @@ int shell_help(char** args)
 int shell_exit(char** args)
 {
 	return 0;
+}
+
+int shell_halt(char** args)
+{
+	system("shutdown -P now");
+	return 1;
 }
 
 /* Part 2 - shell arguments given at start =====================================================================================================
@@ -190,27 +201,67 @@ string make_prompt() {
 
 /* Part 4 - CommandObject class, represents 1 line of command entered by user ================================================
 */
-
 class CommandObject final {
 public:
-	CommandObject(string command, vector<string> arguments,
-		string output_file_name, string input_file_name, std::shared_ptr<CommandObject> reciever) {
-		m_command = command;
-		for (size_t i = 0; i < arguments.size(); i++)
-			m_arguments.push_back(arguments[i]);
-		m_input_file_name = input_file_name;
-		m_output_file_name = output_file_name;
-		m_reciever = reciever;
+	CommandObject(string command_string) {
+		string word = "";
+		bool word_is_input_file = false, word_is_output_file = false, word_is_first = true, after_pipe = false;
+		for (size_t i = 0; i < command_string.size(); i++) {
+			if (command_string[i] == '|') {
+				m_has_pipe = true;
+				after_pipe = true;
+				word_is_first = true;
+				continue;
+			}
+			else if (command_string[i] == ' ') {
+				if (!word.empty()) {
+					if (word_is_first) {
+						if (after_pipe)
+							m_second_command = word;
+						else
+							m_command = word;
+						word_is_first = false;
+					}
+					else if (word_is_input_file) {
+						m_input_file_name = word;
+						m_has_input_file = true;
+						word_is_input_file = false;
+					}
+					else if (word_is_output_file) {
+						m_output_file_name = word;
+						m_has_output_file = true;
+						word_is_output_file = false;
+					}
+					else {
+						if (after_pipe)
+							m_second_arguments.push_back(word);
+						else
+							m_arguments.push_back(word);
+					}
+					word = "";
+				}
+			}
+			else if (command_string[i] == '>')
+				word_is_output_file = true;
+			else if (command_string[i] == '<')
+				word_is_input_file = true;
+			else
+				word += command_string[i];
+		}
 	}
 
-	string m_command;
+	string m_command = "";
 	vector<string> m_arguments;
-	string m_input_file_name;
-	string m_output_file_name;
-	std::shared_ptr<CommandObject> m_reciever;
+	string m_input_file_name = "";
+	string m_output_file_name = "";
+	bool m_has_output_file = false;
+	bool m_has_input_file = false;
+	bool m_has_pipe = false;
+	string m_second_command = "";
+	vector<string> m_second_arguments;
 
 	friend std::ostream& operator<<(std::ostream& lhs, const CommandObject& rhs);
-	
+
 	char* get_command_as_c() {
 		char* c_string = new char[m_command.length() + 1];
 		strcpy(c_string, m_command.c_str());
@@ -219,36 +270,38 @@ public:
 
 	char** get_arguments_as_c() {
 		size_t array_size = m_arguments.size() + 2;
-		/*if (!m_input_file_name.empty())
-			array_size += 2;
-		if (!m_output_file_name.empty())
-			array_size += 2;*/
 		char** arguments = new char* [array_size];
 
 		size_t i = 0;
 		arguments[i] = new char[m_command.length() + 1];
 		strcpy(arguments[i], m_command.c_str());
 		for (i = 0; i < m_arguments.size(); i++) {
-			arguments[i+1] = new char[m_arguments[i].length() + 1];
-			strcpy(arguments[i+1], m_arguments[i].c_str());
+			arguments[i + 1] = new char[m_arguments[i].length() + 1];
+			strcpy(arguments[i + 1], m_arguments[i].c_str());
 		}
 		i++;
-		/*if (!m_input_file_name.empty()) {
-			arguments[i] = new char[2];
-			strcpy(arguments[i], "<");
-			i++;
-			arguments[i] = new char[m_input_file_name.length() + 1];
-			strcpy(arguments[i], m_input_file_name.c_str());
-			i++;
+		arguments[i] = nullptr;
+		return arguments;
+	}
+
+	char* get_second_command_as_c() {
+		char* c_string = new char[m_second_command.length() + 1];
+		strcpy(c_string, m_second_command.c_str());
+		return c_string;
+	}
+
+	char** get_second_arguments_as_c() {
+		size_t array_size = m_second_arguments.size() + 2;
+		char** arguments = new char* [array_size];
+
+		size_t i = 0;
+		arguments[i] = new char[m_second_command.length() + 1];
+		strcpy(arguments[i], m_second_command.c_str());
+		for (i = 0; i < m_second_arguments.size(); i++) {
+			arguments[i + 1] = new char[m_second_arguments[i].length() + 1];
+			strcpy(arguments[i + 1], m_second_arguments[i].c_str());
 		}
-		if (!m_output_file_name.empty()) {
-			arguments[i] = new char[2];
-			strcpy(arguments[i], ">");
-			i++;
-			arguments[i] = new char[m_output_file_name.length() + 1];
-			strcpy(arguments[i], m_output_file_name.c_str());
-			i++;
-		}*/
+		i++;
 		arguments[i] = nullptr;
 		return arguments;
 	}
@@ -262,190 +315,197 @@ std::ostream& operator<<(std::ostream& lhs, const CommandObject& rhs)
 	for (size_t i = 0; i < rhs.m_arguments.size(); i++)
 		lhs << '\"' << rhs.m_arguments[i] << '\"' << ", ";
 	lhs << "\n";
+	if (rhs.m_has_pipe) {
+		lhs << "PIPE TO: \n";
+		lhs << "COMMAND: " << '\"' << rhs.m_second_command << '\"' << "\n";
+		lhs << "ARGUMENTS: ";
+		for (size_t i = 0; i < rhs.m_second_arguments.size(); i++)
+			lhs << '\"' << rhs.m_second_arguments[i] << '\"' << ", ";
+		lhs << "\n";
+	}
 	lhs << "INPUT FILE: " << rhs.m_input_file_name << "\n";
 	lhs << "OUTPUT FILE: " << rhs.m_output_file_name << "\n";
-	if (rhs.m_reciever != nullptr)
-		lhs << "PIPE TO COMMAND : " << rhs.m_reciever.get()->m_command << "\n";
 	return lhs;
 }
 
-vector<CommandObject> createCommandObjects(string input_line) {
 
-	vector<CommandObject> result, other_objects;
+vector<CommandObject> separate_commands(string input_line) {
 	vector<char> delimiters = { ' ','\t','\r','\n','\a' };
 	vector<char> special_characters = { '#',';','<','>','|' };
 	char escape_character = '\\';
-	string first_word = "", word = "" , input_file = "", output_file = "";
-	vector<string> other_words;
-	bool first_done = false, output_file_mode = false, input_file_mode = false, filename_ended = false;
-	std::shared_ptr<CommandObject> sharedObject = nullptr;
-
-
+	string one_command = "";
+	vector<string> all_commands;
+	bool last_was_delimiter = false;
 
 	for (size_t i = 0; i < input_line.size(); i++) {
 		bool is_delimiter = false;
-		for (size_t j = 0; j < delimiters.size(); j++) 
-			if (input_line[i] == delimiters[j])
-				is_delimiter = true;
+		for (size_t j = 0; j < delimiters.size(); j++) {
+			if (input_line[i] == delimiters[j]) {
+				if (!last_was_delimiter) {
+					one_command += " ";
+					last_was_delimiter = true;
 
-		if (!is_delimiter && input_line[i] != ';' && input_line[i] != '|' && filename_ended) break;
-
-		if (is_delimiter) {
-			if ((output_file_mode && !output_file.empty()) || (input_file_mode && !input_file.empty()))
-				filename_ended = true;
-			else{
-				if (word.empty())
-					continue;
-				else {
-					if (first_done)
-						other_words.push_back(word);
-					else {
-						first_word = word;
-						first_done = true;
-					}
-					word = "";
 				}
+				is_delimiter = true;
 			}
 		}
-		else if( input_line[i] == '\\') {
-			if (i + 1 < input_line.size()) {
-				bool next_is_special = false;
-				for (size_t j = 0; j < special_characters.size(); j++)
-					if (input_line[i+1] == special_characters[j])
-						next_is_special = true;
+		if (is_delimiter)
+			continue;
 
-				if (next_is_special) {
-					i++;
-					word += input_line[i];
-					continue;
-				}
-				else {
-					word += '\\';
-					continue;
-				}
-			}
+		if (input_line[i] == '>') {
+			if (last_was_delimiter)
+				one_command += "> ";
+			else
+				one_command += " > ";
+			last_was_delimiter = true;
+			continue;
+		}
+		else if (input_line[i] == '>') {
+			if (last_was_delimiter)
+				one_command += "< ";
+			else
+				one_command += " < ";
+			last_was_delimiter = true;
+			continue;
+		}
+		else if (input_line[i] == '|') {
+			if (last_was_delimiter)
+				one_command += "| ";
+			else
+				one_command += " | ";
+			last_was_delimiter = true;
+			continue;
+		}
+
+		last_was_delimiter = false;
+
+		if (input_line[i] == ';') {
+			one_command += " ";
+			all_commands.push_back(one_command);
+			one_command = "";
 		}
 		else if (input_line[i] == '#') {
 			break;
 		}
-		else if (input_line[i] == ';') {
-			string other_command = input_line.substr(i + 1);
-			other_objects = createCommandObjects(other_command);
-			break;
-		}
-		else if (input_line[i] == '|') {
-			string other_command = input_line.substr(i + 1);
-			other_objects = createCommandObjects(other_command);
-			sharedObject = make_shared<CommandObject>(other_objects[0]);
-			break;
-		}	
+		else if (input_line[i] == '\\') {
+			if (i + 1 < input_line.size()) {
+				bool next_is_special = false;
+				for (size_t j = 0; j < special_characters.size(); j++)
+					if (input_line[i + 1] == special_characters[j])
+						next_is_special = true;
 
-		else if (input_line[i] == '>' && !output_file_mode && !input_file_mode) {
-			output_file_mode = true;
-		}
-		else if (input_line[i] == '<'&& !output_file_mode && !input_file_mode) {
-			input_file_mode = true;
+				if (next_is_special) {
+					i++;
+					one_command += input_line[i];
+					continue;
+				}
+				else {
+					one_command += '\\';
+					continue;
+				}
+			}
 		}
 		else {
-			if (input_file_mode)
-				input_file += input_line[i];
-			else if (output_file_mode)
-				output_file += input_line[i];
-			else
-				word += input_line[i];
+			one_command += input_line[i];
 		}
 	}
-	if (word != "") {
-		if (first_done)
-			other_words.push_back(word);
-		else
-			first_word = word;
+	if (one_command != "" || one_command != " ") {
+		one_command += " ";
+		all_commands.push_back(one_command);
 	}
-	
-	if (!first_word.empty()) {
-		CommandObject commandObject{ first_word,other_words,output_file,input_file,sharedObject };
-		result.push_back(commandObject);
-	}
-	for (size_t i = 0; i < other_objects.size(); i++)
-		result.push_back(other_objects[i]);
 
-	return result;
+	vector<CommandObject> command_objects;
+	for (size_t i = 0; i < all_commands.size(); i++)
+		command_objects.push_back({ all_commands[i] });
+	return command_objects;
 }
 
 /* Part 5 - command execution ========================================================================
 */
 
-bool execute_command(CommandObject command) {
-	int pid, wpid;
-	int status;
-	char* command_as_c = command.get_command_as_c();
-	char** arguments_as_c = command.get_arguments_as_c();
-	int i = 0;
-
-	for (i = 0; i < shell_num_builtins(); i++) {
-		if (command.m_command == builtin_str[i]) {
+int execute_commands(CommandObject command) {
+	/*for (i = 0; i < shell_num_builtins(); i++) {
+		if (strcmp(command_as_c, builtin_str[i]) == 0) {
 			return (*builtin_func[i])(arguments_as_c);
 		}
-	}
-	i = 0;
+	}*/
+
+	int pid, wpid, status, file_desc, file_desc2;
+	int pipefd[2];
+	pipe(pipefd);
+
 	pid = fork();
 	if (pid == 0) {
-		// Child process
-		if (execvp(command.get_command_as_c(), command.get_arguments_as_c()) == -1) {
-			perror("shell");
+		// child process
+		if (command.m_has_pipe) {
+			// replace standard input with input part of pipe
+			if (dup2(pipefd[0], 0) < 0)
+				perror("dub2child");
+			// close unused hald of pipe
+			if (close(pipefd[1]) < 0)
+				perror("closechild");
+
+			if (command.m_has_output_file) {
+				file_desc = open(command.m_output_file_name.c_str(), O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
+				dup2(file_desc, 1);
+			}
+
+			if (command.m_has_input_file) {
+				file_desc2 = open(command.m_input_file_name.c_str(), O_RDONLY);
+				dup2(file_desc2, 0);
+			}
+
+			if (execvp(command.get_second_command_as_c(), command.get_second_arguments_as_c()) == -1)
+				perror("shell");
 		}
-		delete[] command_as_c;
-		while (true) {
-			if (arguments_as_c[i] == NULL)
-				break;
-			delete[] arguments_as_c[i];
-			i++;
+		else {
+			if (command.m_has_output_file) {
+				file_desc = open(command.m_output_file_name.c_str(), O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
+				dup2(file_desc, 1);
+			}
+
+			if (command.m_has_input_file) {
+				file_desc2 = open(command.m_input_file_name.c_str(), O_RDONLY);
+				dup2(file_desc2, 0);
+			}
+
+			if (execvp(command.get_command_as_c(), command.get_arguments_as_c()) == -1)
+				perror("shell");
+
 		}
-		delete[] arguments_as_c;
 		exit(-1);
+		//return 1;
 	}
-	else if (pid < 0) {
-		// Error forking
-		perror("shell");
-	}
-	else {
+	else if (pid > 0) {
 		// Parent process
+
+		if (command.m_has_pipe) {
+			// replace standard output with output part of pipe
+			if (dup2(pipefd[1], 1) < 0)
+				perror("dup2parent");
+			// close unused unput half of pipe
+			if (close(pipefd[0]) < 0)
+				perror("closeparent");
+
+			if (execvp(command.get_command_as_c(), command.get_arguments_as_c()) == -1)
+				perror("shell");
+		}
 		do {
 			wpid = waitpid(pid, &status, WUNTRACED);
 		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
 	}
-	
-	delete[] command_as_c;
-	i = 0;
-	while (true) {
-		if (arguments_as_c[i] == NULL)
-			break;
-		delete[] arguments_as_c[i];
-		i++;
-	}
-	delete[] arguments_as_c;
-	return true;
-}
-
-bool execute_line(vector <CommandObject> commands) {
-	bool error = false;
-	for (size_t i = 0; i < commands.size(); i++) {
-		error = execute_command(commands[i]);
-		if (error)
-			return false;
-	}
-	return true;
+	return 1;
 }
 
 /* Part 6 - server mode ==================================================================================
 */
 
 //server from classmate
-
+/*
 int launch_as_server() {
 	int i, s, ns, r;
 	fd_set rs;
-	char buff[64], msg[] = "server ready\n";
+	char buff[1000], msg[1000] = "server ready\n";
 	struct sockaddr_un ad;
 
 	memset(&ad, 0, sizeof(ad));
@@ -475,9 +535,21 @@ int launch_as_server() {
 	{
 		buff[r] = 0;			// za poslednym prijatym znakom
 		printf("received: %d bytes, string: %s\n", r, buff);
-		for (i = 0; i < r; i++) buff[i] = toupper(buff[i]);
-		printf("sending back: %s\n", buff);
-		write(ns, buff, r);		// zaslanie odpovede
+
+		//toto som menil oproti povodnemu
+		vector<CommandObject> commands;
+		commands = createCommandObjects(string{ buff });
+		int exit = execute_line(commands);
+		
+		FILE* stream;
+		stream = fopen("TEMP_TEXT_OUTPUT.txt", "r");
+		int count = fread(&msg, sizeof(char), 1000, stream);
+		fclose(stream);
+
+		// Printing data to check validity
+
+		printf("sending back: %s\n", msg);
+		write(ns, msg, r);		// zaslanie odpovede
 	}
 	perror("read");	// ak klient skonci (uzavrie soket), nemusi ist o chybu
 
@@ -485,14 +557,14 @@ int launch_as_server() {
 	close(s);
 	return 0;
 }
-
+*/
 /* Part 7 - client mode ========================================================================================
 */
-
+/*
 int launch_as_client() {
 	int s, r;
 	fd_set rs;	// deskriptory pre select()
-	char msg[64] = "hello world!";
+	char msg[1000] = "hello world!";
 	struct sockaddr_un ad;
 
 	memset(&ad, 0, sizeof(ad));
@@ -533,22 +605,21 @@ int launch_as_client() {
 	close(s);
 	return 0;
 }
-
+/
 /*Part 8 - Standalone mode =================================================================================
 */
-int launch_as_standalone() {
-	bool exit = false;
-	while (!exit) {
-		cout << make_prompt();
+void launch_as_standalone() {
+	int is_exit = 1;
+	while (true) {
 		string input_line;
 		vector<CommandObject> commands;
 		getline(std::cin, input_line);
-		commands = createCommandObjects(input_line);
-		exit = execute_line(commands);
-		//for (size_t i = 0; i < commands.size(); i++)
-			//cout << i << ":\n" << commands[i] << "\n";
+		commands = separate_commands(input_line);
+
+		for (size_t i = 0; i < commands.size(); i++) {
+			is_exit = execute_commands(commands[i]);
+		}
 	}
-	return 0;
 }
 
 /*Part 9 - main function ==============================================================================
@@ -570,14 +641,14 @@ int main(int argc, char* argv[]) {
 		shell_help(argv);
 	}
 	else if (shell_arguments.mode == "STANDALONE" || shell_arguments.mode == "") {
-		success = launch_as_standalone();
+		launch_as_standalone();
 	}
-	else if (shell_arguments.mode == "SERVER") {
-		success = launch_as_server();
+	/*else if (shell_arguments.mode == "SERVER") {
+		launch_as_server();
 	}
 	else if (shell_arguments.mode == "CLIENT") {
-		success = launch_as_client();
-	}
-	
-	return success;
+		launch_as_client();
+	}*/
+	cout << "Shell ended\n";
+	return 0;
 }
